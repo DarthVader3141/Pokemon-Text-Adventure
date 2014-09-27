24ca2d3d8134b7c863d08bbff5f7957bf6568015
DarthVader3141 <rgovindjee@gmail.com>
2014-09-26 17:12:15 -0700

    Renamed PokemonList.java to StatsFile.java, added a method to return a numbered line from the text file as a string.

---
+++ src/StatsFile.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class StatsFile {
+	
+
+	public String getLine(int line) throws IOException{
+		
+		String linetoread = null;
+/*		String pokemon1;
+		
+		Scanner fileScan,pokemonScan;
+		fileScan = new Scanner(new File("PokemonListGen1"));
+		
+		while (fileScan.hasNext()){
+			
+			pokemon1 = fileScan.nextLine();
+			pokemonScan=new Scanner(pokemon1);
+			pokemonScan.useDelimiter("     ");
+			while(pokemonScan.hasNext()){
+				System.out.print("   " + pokemonScan.next());
+			}
+			*/
+		FileInputStream fs= new FileInputStream("PokemonListGen1");
+		BufferedReader br = new BufferedReader(new InputStreamReader(fs));
+			
+		while (line < 30) {
+			br.readLine();
+			linetoread = br.readLine();
+			line++;
+		}
+		
+		return linetoread;
+	}
+}
